71dd24a44372904423a46a904374e94cc5f5a24f
Hector Vergara <hector.vergara416@gmail.com>
2020-11-14 13:21:37 +0800

    style: add css styling to webpage

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+function draw(timestamp) {
+  if (!startTime) {
+    startTime = timestamp;
+   }
+ 
+   rotateCount = (timestamp - startTime) / 3;
+
+  if (rotateCount > 359) {
+    rotateCount %= 360;
+  }
+
+  spinner.style.transform = `rotate(${rotateCount}deg)`;
+  rAF = requestAnimationFrame(draw);
+}
+
+const spinner = document.querySelector('div');
+let rotateCount = 0;
+let startTime = null;
+let rAF;
+
+draw();
